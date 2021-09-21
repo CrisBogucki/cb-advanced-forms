6e98a599c992c2909e3390a9ec65e6e376cc2eb3
Krzysztof Bogucki <krzysztof.bogucki@asseco.pl>
2021-09-21 21:05:13 +0200

    add fields styles and fonts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { DesignSystemModule } from './design-system/design-system.module';
+import { DesignSystemModule } from './@design-system/design-system.module';
 
 @NgModule({
   declarations: [
